8a81fbe5878521ea87b21c246b36fbe9a9637083
Gustavo <gustavo.prog40@gmail.com>
2022-07-22 18:47:32 -0300

    serach bar responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,26 @@
-import { Container } from './style'
+import { Container, Button, Input, Form } from './style'
+import logo from '../../assets/logo.png'
+import lupa from '../../assets/lupa.png'
 
 function Header() {
   return (
     <Container>
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <h3>HEADER</h3>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={logo} alt="logo" width="42" height="40" />
+        <Form>
+          <Input type="search" name="search" placeholder="Search..." />
+          <Button type="submit">
+            <img
+              src={lupa}
+              width="32"
+              alt="lupa"
+            />
+          </Button>
+        </Form>
       </div>
+      <div style={{ display: 'flex', width: '25%' }}></div>
+      <h3>HOME</h3>
+      <h3>REGISTER</h3>
     </Container>
   )
 }
--- src/components/header/style.jsx
+++ src/components/header/style.jsx
@@ -2,10 +2,42 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-  /* align-items: flex-start; */
-  justify-content: center;
+  align-items: center;
+  justify-content: space-around;
   background: #e6e6e6;
   width: 100%;
   height: 4.5rem;
   border-bottom: solid 1px #b3b3b3;
+`
+export const Form = styled.form`
+  display: flex;
+  margin-left: 13px;
+  align-items: center;
+
+  
+`
+export const Input = styled.input`
+  display: flex;
+  width: 20rem;
+  height: 2.4rem;
+  border-radius: 5px;
+  padding-left: 10px;
+  `
+
+
+export const Button = styled.button`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 35px;
+height: 38px;
+margin-left: 3px;
+background: #e6e6e6;
+cursor: pointer;
+transition: ease 0.7s;
+
+&:hover{
+  background: darkgray;
+}
+
 `
