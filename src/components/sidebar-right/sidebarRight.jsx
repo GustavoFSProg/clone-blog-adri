1cb8ac00e69dc8553c712005aac3d7e85ac5071b
Gustavo <gustavo.prog40@gmail.com>
2022-07-26 15:17:13 -0300

    news

--- src/components/sidebar-right/sidebarRight.jsx
+++ src/components/sidebar-right/sidebarRight.jsx
@@ -1,7 +1,13 @@
-import {Container} from './style'
+import { Container } from './style'
+import imagem from '../../assets/left-image.jpeg'
 
 function SidebarRight(){
-  return (<Container><h2>Sidebar - Right</h2></Container>)
+  return (<Container>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '210px', alignItems: 'center', justifyContent: 'center' }}>
+      <img src={imagem} alt="imagem" width="222" height="101" />
+      <h2 style={{ fontSize: '22px', width: '85%', letterSpacing: '1px', fontFamily: 'Roboto', color: '#006bb3' }}>Android App and Site https</h2>
+    </div>
+  </Container>)
 }
 
 export default SidebarRight
