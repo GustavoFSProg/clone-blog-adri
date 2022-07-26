1ae34d8d70f2de102f49d5a9b88d7d6bfdc792e3
Gustavo <gustavo.prog40@gmail.com>
2022-07-26 13:00:27 -0300

    side bar

--- src/components/sidebar-left/sidebar-left.jsx
+++ src/components/sidebar-left/sidebar-left.jsx
@@ -1,10 +1,33 @@
-import { Container } from './style'
+import { Container, Button, ButtonDois } from './style'
 
 function SidebarLeft() {
   return (
     <Container>
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <h2>Sidebar - Left</h2>
+      <div style={{ ButtonDois, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <p
+            style={{ width: '215px', lineHeight: '24px', fontSize: '0.8rem', fontFamily: 'Roboto' }}
+          >
+            <h2>DEVIL Place is a community of 881,454 amazing developers</h2>
+          </p>
+          <span
+            style={{ width: '215px', lineHeight: '21px', fontSize: '1rem', fontFamily: 'Roboto' }}
+          >
+            We're a place where coders share, stay up-to-date and grow their careers.
+          </span>
+          <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+            <Button style={{}}>CREATE ACCOUNT</Button>
+
+            <ButtonDois>Login</ButtonDois>
+          </div>
+        </div>
       </div>
     </Container>
   )
--- src/components/sidebar-left/style.jsx
+++ src/components/sidebar-left/style.jsx
@@ -11,7 +11,43 @@ export const Container = styled.div`
   margin-left: 4rem;
   margin-top: 20px;
 
-  @media screen and (max-width: 850px){
+  @media screen and (max-width: 850px) {
     display: none;
+  }
+`
+export const Button = styled.button`
+  border-radius: 8px;
+  margin-bottom: 5px;
+  border: 1px solid #0052cc;
+  height: 2.6rem;
+  width: 100%;
+  color: #0052cc;
+  font-family: Roboto;
+  margin-top: 21px;
+  transition: ease 0.6s;
+  cursor: pointer;
+
+  &:hover{
+    background: #0052cc;
+    color: white
+  }
+`
+export const ButtonDois = styled.button`
+  border-radius: 8px;
+  margin-bottom: 10px;
+  border: none;
+  height: 2.6rem;
+  width: 100%;
+  color: gray;
+  font-family: Roboto;
+  font-size: 17px;
+  margin-top: 1px;
+  transition: ease 0.6s;
+  cursor: pointer;
+
+
+  &:hover{
+    background: gray;
+    color: white
   }
 `
