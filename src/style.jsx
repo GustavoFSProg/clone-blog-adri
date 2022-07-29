1ea6c0756c6b6481e263e91ef99312a147aab941
Gustavo <gustavo.prog40@gmail.com>
2022-07-29 19:11:58 -0300

    mmmnnnmmmnn

--- src/style.jsx
+++ src/style.jsx
@@ -61,7 +61,7 @@ export const ContainerWrapper = styled.div`
   height: auto;
   background: #f2f2f2;
   /* background: green; */
-  margin-top: 20rem;
+  margin-top: 18rem;
   display: flex;
   justify-content: center;
   flex-direction: column;
